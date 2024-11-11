f0a110accc0b06d8cbb97a565c97b6f87e82a3bb
AliTarani <a.tarani1999@gmail.com>
2024-11-11 18:58:37 +0330

    Add product Hook to the project

---
+++ src/hooks/useProducts.ts
@@ -0,0 +1,23 @@
+import { useEffect, useState } from "react";
+import crudService from "../services/crud.service";
+import Product from "../types/product.type";
+
+const productService = crudService("/products");
+
+const useProducts = () => {
+  const [products, setProducts] = useState<Product[]>([]);
+
+  useEffect(() => {
+    const { request, cancel } = productService.getAll<Product>();
+    request.then((data) => {
+      setProducts(data);
+    });
+
+    return () => {
+      cancel();
+    };
+  }, []);
+  return { products, setProducts };
+};
+
+export default useProducts;
