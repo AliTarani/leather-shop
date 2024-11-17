857828c3c10a8eb9ee849c7fc13726b719eb52f2
AliTarani <a.tarani1999@gmail.com>
2024-11-17 09:13:28 +0330

    Add modal component.

---
+++ src/components/modal/Modal.tsx
@@ -0,0 +1,18 @@
+import "./Modal.css";
+
+const Modal = ({ isOpen, onClose, children }) => {
+  if (!isOpen) return null; // Don't render modal if it's not open
+
+  return (
+    <div className="modal-overlay" onClick={onClose}>
+      <div className="modal-content" onClick={(e) => e.stopPropagation()}>
+        <button className="close-btn" onClick={onClose}>
+          X
+        </button>
+        {children}
+      </div>
+    </div>
+  );
+};
+
+export default Modal;
