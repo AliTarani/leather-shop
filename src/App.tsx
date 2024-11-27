21fc9bb4a5680b7ce9f20102474bcc35228f4f4a
AliTarani <a.tarani1999@gmail.com>
2024-11-27 12:03:25 +0330

    Add global loading to application.

--- src/App.tsx
+++ src/App.tsx
@@ -1,35 +1,28 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import "./App.css";
+
+let MyRes: any;
+
+const delayPromise = new Promise((resolve) => {
+    setTimeout(() => {
+      resolve("Resolved after 3 seconds");
+    }, 3000);
+  }).then((res) => {
+    MyRes = res;
+  });
 
 function App() {
-  const [count, setCount] = useState(0)
+  
+
+  if (!MyRes) {
+    throw delayPromise;
+  }
 
   return (
     <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <h1> App </h1>
+      {MyRes}
     </>
-  )
+  );
 }
 
-export default App
+export default App;
--- src/main.tsx
+++ src/main.tsx
@@ -1,13 +1,16 @@
-import { StrictMode } from "react";
+import { StrictMode, Suspense } from "react";
 import { createRoot } from "react-dom/client";
 import "./index.css";
 import App from "./App.tsx";
 import ContextProvider from "./startup/contextProvider.tsx";
+import Loading from "./components/loading.tsx";
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
     <ContextProvider>
-      <App />
+      <Suspense fallback={<Loading />}>
+        <App />
+      </Suspense>
     </ContextProvider>
   </StrictMode>
 );
