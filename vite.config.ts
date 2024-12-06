ba3b6b0d73a0068612b819d56be5b36a864f9a10
AliTarani <a.tarani1999@gmail.com>
2024-12-06 13:54:08 +0330

    Add environment system in app help vite.

--- vite.config.ts
+++ vite.config.ts
@@ -1,12 +1,26 @@
-import { defineConfig } from "vite";
+import { defineConfig, loadEnv } from "vite";
 import react from "@vitejs/plugin-react";
 import * as path from "path";
 // https://vite.dev/config/
-export default defineConfig({
-  plugins: [react()],
-  resolve: {
-    alias: [
-      { find: "@core", replacement: path.resolve(__dirname, "src/core") },
-    ],
-  },
+const cherryPickedKeys = [
+  "REACT_APP_APP_NAME",
+  "REACT_APP_ENV",
+  "REACT_APP_API_URL",
+];
+
+export default defineConfig(({ mode }) => {
+  const env = loadEnv(mode, process.cwd(), "");
+  const processEnv = {};
+  cherryPickedKeys.forEach((key) => (processEnv[key] = env[key]));
+  return {
+    define: {
+      "process.env": processEnv,
+    },
+    resolve: {
+      alias: [
+        { find: "@core", replacement: path.resolve(__dirname, "src/core") },
+      ],
+    },
+    plugins: [react()],
+  };
 });
